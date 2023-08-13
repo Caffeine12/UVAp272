8505083fc16e1e3f85f381f42e60abe4c490c446
Caffeine12 <jubairku12@gmail.com>
2023-08-13 09:27:11 +0200

    first commit

---
+++ src/App.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception 
+    {
+        Scanner sc = new Scanner(System.in);
+        String input=sc.nextLine();
+        int flag=0;
+        char[] ch=input.toCharArray();
+
+        for(char c:ch)
+        {
+            if(c=='"')
+            {
+                if(flag==0)
+                {
+                    System.out.print("``");
+                    flag=1;
+                }
+                else
+               {   
+                    System.out.print("''");
+                    flag=0;
+                }     
+            }
+            else
+            System.out.print(c);                         
+        }         
+    }
+}
